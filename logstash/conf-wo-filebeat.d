7e8f367c70d74f59ae1af5a60e74cc76d273f30e
bharath.r <bharath.r>
2022-08-16 12:31:42 +0530

    1. Correction for dockercompose & logstash conf 2. populate apache logs

--- logstash/conf-wo-filebeat.d
+++ logstash/conf-wo-filebeat.d
@@ -3,10 +3,15 @@ input{
         path => "/usr/share/logstash/custom-config/apache-logs-small"
         start_position => "beginning"
         discover_interval => "10"
-        # sincedb_path => "/dev/null"
+        sincedb_path => "/dev/null"
         stat_interval => "1 second"
     }
 }
+
+# note: 
+# 1. Be careful while setting delimiter as it causes problems while parsing files (default value is "\n" which is good enough for most cases)
+# 2. The `sincedb_path` parameter trakcs which was the last point at which reading was done from the file. Setting it to `sincedb_path => "/dev/null"` ensures that everytime file is read afresh (which is useful incase of debugging or in cases  like deleting the index from ES & wanting the data to be re-populated)
+
 
 filter
 {
@@ -45,7 +50,7 @@ output{
   elasticsearch {
     index => "apache_elastic_example"
     hosts => ["http://elasticsearch:9200"]
-    template => "/usr/share/logstash/custom-config/apache_logs_schema.json"
+    template => "/usr/share/logstash/custom-config/logstash/apache_logs_schema.json"
     template_name => "apache_logs_es_mappings"
     template_overwrite => true
   }
